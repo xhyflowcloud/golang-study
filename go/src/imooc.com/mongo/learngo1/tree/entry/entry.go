4d0e60afdf2d9378bd6c620ec90f060882e3ac7c
xiahongyun <xiahongyun@jd.com>
2018-11-24 20:19:56 +0800

    oriented object

---
+++ go/src/imooc.com/mongo/learngo1/tree/entry/entry.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tools/container/intsets"
+	"imooc.com/mongo/learngo1/tree"
+)
+
+type MyTreeNode struct {
+	node *tree.Node
+}
+
+func (myTreeNode *MyTreeNode) postOrder()  {
+	if myTreeNode == nil || myTreeNode.node == nil {
+		return
+	}
+	left := MyTreeNode{myTreeNode.node.Left}
+	right := MyTreeNode{myTreeNode.node.Right}
+	left.postOrder()
+	right.postOrder()
+	myTreeNode.node.Print()
+}
+
+func testSparse()  {
+	s := intsets.Sparse{}
+
+	s.Insert(1)
+	s.Insert(1000)
+	s.Insert(1000000)
+	fmt.Println(s.Has(1000))
+	fmt.Println(s.Has(10000))
+}
+ func main() {
+	var root tree.Node
+	fmt.Println(root)
+
+	root = tree.Node{Value: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{5, nil, nil}
+	root.Right.Left = new(tree.Node)
+	root.Right.Right = tree.CreateNode(2)
+
+	root.Right.Right.SetValue(3)
+	root.Right.Right.Print()
+
+	/*nodes := []Node {
+		{value: 3},
+		{},
+		{6, nil, nil},
+	}*/
+	//root.Traverse()
+	var myTreeNode MyTreeNode
+	myTreeNode.node = &root
+	myTreeNode.postOrder()
+
+
+	testSparse()
+}
---
+++ go/src/imooc.com/mongo/learngo1/tree/node.go
@@ -0,0 +1,47 @@
+package tree
+
+import "fmt"
+
+type Node struct {
+	Value int
+	Left, Right *Node
+}
+
+func (node Node) Print() {
+	fmt.Println(node.Value)
+}
+
+func (node *Node) SetValue(value int) {
+	if node == nil {
+		fmt.Println("Setting value to nil node. Ignored!")
+		return
+	}
+	node.Value = value
+}
+
+func CreateNode(value int) *Node {
+	return &Node{Value: value}
+}
+
+func main() {
+	var root Node
+	fmt.Println(root)
+
+	root = Node{Value: 3}
+	root.Left = &Node{}
+	root.Right = &Node{5, nil, nil}
+	root.Right.Left = new(Node)
+	root.Right.Right = CreateNode(2)
+
+	root.Right.Right.SetValue(3)
+	root.Right.Right.Print()
+
+	/*nodes := []Node {
+		{value: 3},
+		{},
+		{6, nil, nil},
+	}*/
+	root.Traverse()
+	var pRoot *Node
+	pRoot.SetValue(200)
+}
